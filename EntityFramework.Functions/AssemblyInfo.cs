320175b5ef7aac51315f206c5859d539f41f5ccf
Dixin Yan <dixinyan@live.com>
2017-05-29 14:52:35 -0700

    Sign assemblies with strong named key.

--- EntityFramework.Functions/AssemblyInfo.cs
+++ EntityFramework.Functions/AssemblyInfo.cs
@@ -5,4 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EntityFramework.Functions")]
 [assembly: Guid("20128f9b-b68d-4e51-a0e9-67eb1e7b9402")]
 
-[assembly: InternalsVisibleTo("EntityFramework.Functions.Tests")]
+[assembly: InternalsVisibleTo("EntityFramework.Functions.Tests,PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100a76dfa6b71ce48" +
+    "6ec3d6c7d1d0f7830b54cb83b2c066daef7c0bb04568bb1cf36c2cb8c34f6ec51dd64454f44cd7" +
+    "0513aa57350ef125ccf25fa4ae2cf064df1bbe93fa3c0df4a6438b5dffd5ef55daefdb740b99cb" +
+    "121cca42f262dd5cb20061cdc77139b068d358601c6c9502e9cd4d477b64bfa1a6cf92c709b2da" +
+    "5549c095")]
